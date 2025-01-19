7495142347c8c8c6962706afa68ded68e6af11c1
Dennison Bertram <dennison@dennisonbertram.com>
2025-01-18 21:53:49 -0500

    fix: change pagination to page in getAddressDAOProposals query

--- src/services/addresses/getAddressDAOProposals.ts
+++ src/services/addresses/getAddressDAOProposals.ts
@@ -1,44 +1,43 @@
 import { GraphQLClient } from 'graphql-request';
 import { GET_ADDRESS_DAO_PROPOSALS_QUERY } from './addresses.queries.js';
 import { getDAO } from '../organizations/getDAO.js';
-import { AddressDAOProposalsInput, AddressDAOProposalsResponse } from './addresses.types.js';
+import { AddressDAOProposalsInput } from './addresses.types.js';
 
 export async function getAddressDAOProposals(
   client: GraphQLClient,
   input: AddressDAOProposalsInput
-): Promise<AddressDAOProposalsResponse> {
+): Promise<Record<string, any>> {
   try {
     if (!input.address) {
       throw new Error('Address is required');
     }
 
-    if (!input.governorId && !input.organizationSlug) {
-      throw new Error('Either governorId or organizationSlug is required');
+    if (!input.organizationSlug) {
+      throw new Error('organizationSlug is required');
     }
 
-    // Get governorId from organizationSlug if provided
-    let governorId = input.governorId;
-    if (!governorId && input.organizationSlug) {
-      const dao = await getDAO(client, input.organizationSlug);
-      if (dao.governorIds && dao.governorIds.length > 0) {
-        governorId = dao.governorIds[0];
-      } else {
-        throw new Error('No governor IDs found for the given organization');
-      }
+    // Get governorId from organizationSlug
+    const dao = await getDAO(client, input.organizationSlug);
+    if (!dao.governorIds?.length) {
+      throw new Error('No governor IDs found for the given organization');
     }
+    const governorId = dao.governorIds[0];
 
-    const response = await client.request<AddressDAOProposalsResponse>(GET_ADDRESS_DAO_PROPOSALS_QUERY, {
-      input: {
-        filters: {
-          governorId
+    const response = await client.request(
+      GET_ADDRESS_DAO_PROPOSALS_QUERY,
+      {
+        input: {
+          filters: {
+            governorId
+          },
+          page: {
+            limit: input.limit || 20,
+            afterCursor: input.afterCursor
+          }
         },
-        pagination: {
-          limit: input.limit || 20,
-          afterCursor: input.afterCursor
-        }
-      },
-      address: input.address
-    });
+        address: input.address
+      }
+    ) as Record<string, any>;
 
     return response;
   } catch (error) {
